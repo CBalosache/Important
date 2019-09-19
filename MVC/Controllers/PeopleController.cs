79671dd6263d8619d47bd85d7b0a0446e0b3138d
Cosmin Balosache <cbalosache@spartaglobal.com>
2019-09-19 09:14:01 +0100

    Important Connection

---
+++ MVC/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Book()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Customer
+    {
+        private string firstName = "";
+        private string lastName = "";
+        private int age = 0;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
